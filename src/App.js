1ebd3106f5c8912bbec3df1b50e30d16cde478db
Merchuli <30795240+merchuli@users.noreply.github.com>
2023-01-30 15:14:22 +0800

    feat: Practice dynamic values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to my blog.';
+  const likes = 50;
+  const link = 'https://www.google.com/';
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked {likes} times</p>
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
